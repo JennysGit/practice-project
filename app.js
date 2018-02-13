2c209ff2ca138b48be87213c7fc8f5b9befeb581
RicoLiu <2869183609@qq.com>
2018-02-13 09:59:02 +0800

    联系爬虫

--- app.js
+++ app.js
@@ -1,31 +1,64 @@
-var WebpackDevServer = require("webpack-dev-server");
-var webpack = require("webpack");
-var webpackCfg = require('./webpack.config.build.js');
+const express = require('express');
+const cheerio = require('cheerio');
+const superagent = require('superagent');
 
-var compiler = webpack(webpackCfg);
+const app = express();
 
-var server = new WebpackDevServer(compiler, {
-  // webpack-dev-server options
-  contentBase: path.join(__dirname, "dist"),
-  hot: true,
-  historyApiFallback: false,
-  compress: true,
-  proxy: {
-    "**": "http://localhost:8080"
-  },
-  clientLogLevel: "info",
-  // webpack-dev-middleware options
-  quiet: false,
-  noInfo: false,
-  lazy: true,
-  filename: "bundle.js",
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000
-  },
-  // It's a required option.
-  publicPath: "/assets/",
-  headers: { "X-Custom-Header": "yes" },
-  stats: { colors: true }
+app.get('/', (req, res, next) => {
+  superagent.get('http://h5.izhangchu.com/dishes_view/index.html?&dishes_id=274')
+    .end((err, sres) => {
+      if (err) {
+        return next(err);
+      }
+      let $ = cheerio.load(sres.text);
+
+      let obj = {}; // 标题
+      $('#DishesView-wrap .goods-desc').each((idx, element) => {
+        let $element = $(element);
+        obj = {
+          title: $element.children('h5').text(),
+          subtitle: $element.children('p').text()
+        };
+      });
+
+      let items = []; // 相关常识
+      $('#DishesCommensense-wrap li').each((idx, element) => {
+        let $element = $(element);
+        items.push({
+          title: $element.children('h5').text(),
+          txt: $element.children('.txt').text().trim()
+        });
+      });
+
+      let Ingredients = []; // 食材
+      $('#DishesMaterial-wrap li').each((idx, element) => {
+        let $element = $(element);
+        Ingredients.push({
+          name: $element.text().trim(),
+          number: $element.children('span').text().trim()
+        });
+      });
+
+      let labels = []; // 标签
+      $('#linksWrap').each((idx, element) => {
+        let $element = $(element);
+        console.log($element.children('li').children('.label-bar').children('a').children('span').text());
+        // labels.push($element.children('a').text().trim());
+      });
+
+      let result = {
+        title: obj.title,
+        subtitle: obj.subtitle,
+        content: items, 
+        Ingredient: Ingredients,
+        label: labels
+      };
+
+      res.send(result);
+    });
 });
-server.listen(8080, "localhost", function() {});
+
+
+app.listen(3000, () => {
+  console.log('app is listening at port 3000');
+});
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var webpack = require('atool-build/lib/webpack');
-
-module.exports = function(webpackConfig) {
-  // 添加一个plugin
-  webpackConfig.babel.plugins.push('transform-runtime');
-  // 返回 webpack 配置对象
-  return webpackConfig;
-};
-
